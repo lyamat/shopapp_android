bf41883bc75fb54290ae5427eaef51f93a66340c
lyamat <ilyamatveev12902@gmail.com>
2024-02-20 16:32:16 +0300

    Tab layout

---
+++ app/src/main/java/com/example/kelineyt/fragments/categories/CupboardFragment.kt
@@ -0,0 +1,4 @@
+package com.example.kelineyt.fragments.categories
+
+class CupboardFragment : BaseCategoryFragment() {
+}
---
+++ app/src/main/java/com/example/kelineyt/fragments/categories/FurnitureFragment.kt
@@ -0,0 +1,4 @@
+package com.example.kelineyt.fragments.categories
+
+class FurnitureFragment : BaseCategoryFragment() {
+}
