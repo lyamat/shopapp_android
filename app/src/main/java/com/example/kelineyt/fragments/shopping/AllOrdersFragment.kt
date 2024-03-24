51905eea70d02987caaf8526fa5f626aad47dcdc
lyamat <ilyamatveev12902@gmail.com>
2024-03-24 22:20:58 +0300

    all orders

---
+++ app/src/main/java/com/example/kelineyt/fragments/shopping/AllOrdersFragment.kt
@@ -0,0 +1,75 @@
+package com.example.kelineyt.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kelineyt.adapters.AllOrdersAdapter
+import com.example.kelineyt.databinding.FragmentAllOrdersBinding
+import com.example.kelineyt.util.Resource
+import com.example.kelineyt.viewmodel.AllOrdersViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+@AndroidEntryPoint
+class AllOrdersFragment : Fragment() {
+    private lateinit var binding: FragmentAllOrdersBinding
+    private val viewModel by viewModels<AllOrdersViewModel>()
+    val allOrdersAdapter by lazy { AllOrdersAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAllOrdersBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOrdersRv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.allOrders.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarAllOrders.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressbarAllOrders.visibility = View.GONE
+                        allOrdersAdapter.differ.submitList(it.data)
+                        if (it.data.isNullOrEmpty()){
+                            binding.tvEmptyOrders.visibility = View.VISIBLE
+                        }
+                    }
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        binding.progressbarAllOrders.visibility = View.GONE
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        allOrdersAdapter.onClick = {
+            val action = AllOrdersFragmentDirections.actionAllOrdersFragmentToOrderDetailFragment(it)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupOrdersRv() {
+        binding.rvAllOrders.apply {
+            adapter = allOrdersAdapter
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        }
+    }
+}
